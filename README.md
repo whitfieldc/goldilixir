9739c4df131c6783d6cf5559dd563cfe21443b9c
Charles <whitfield.cw@gmail.com>
2015-10-25 20:12:36 -0700

    add poison library and filter API hits to perfect rhymes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Goldilixir
 ==========
 
-** TODO: Add description **
+#### Pun-generating app following the Goldilocks concept by [gabebw](http://gabebw.com/blog/2015/10/24/how-to-learn-a-new-programming-language)
--- lib/goldilixir.ex
+++ lib/goldilixir.ex
@@ -1,2 +1,22 @@
 defmodule Goldilixir do
+  def find_perfect_rhymes(keyword) do
+    resp = Rhymebrain.get!(keyword).body
+    |> Enum.filter(fn(word_match) -> word_match["score"] >= 300 end)
+    # |> IO.inspect
+  end
 end
+
+
+defmodule Rhymebrain do
+  use HTTPoison.Base
+
+  def process_url(keyword) do
+    "http://rhymebrain.com/talk?function=getRhymes&word=" <> keyword
+  end
+
+  def process_response_body(body) do
+    Poison.Parser.parse!(body)
+  end
+
+end
+
